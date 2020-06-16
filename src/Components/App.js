fac8146e0bd01e7dd5ccb827a12ea5dc7f3d8c82
Todd Fleeman <rtoddf@me.com>
2020-06-16 00:53:40 -0400

    styling first pass

--- src/Components/App.js
+++ src/Components/App.js
@@ -9,37 +9,27 @@ class App extends React.Component {
 
   constructor(props){
     super(props);
-    this.state=({index:0,category:['All','Lorem','Ipsum','Dolor']})
+    this.state=({ index:0, category:['All', 'Lorem', 'Ipsum', 'Dolor'] })
   }
 
   componentDidMount(){
-    this.swiper=new Swiper('.swiper-container',{
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows : true,
-      },
+    this.swiper=new Swiper('.swiper-container',
+    {
       pagination: {
         el: '.swiper-pagination',
       },
       navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      }
-    })
+      },
+      // spaceBetween: 30,
+    });
   }
 
   render() {
     return (
       <div className="App">
-        <div className="filterContainer">
-          {/* This will be a filter to apply a filter function to swiper slides. Very basic filter :)*/}
+        {/* <div className="filterContainer">
           <div className="filterGoLeft"
           onClick={()=>{
             this.setState({index:this.state.index-1<0?this.state.category.length-1:this.state.index-1},
@@ -52,27 +42,38 @@ class App extends React.Component {
             ()=>this.swiper.update())
           }}
           >&gt;</div>
-        </div>
+        </div> */}
         <div className="swiperMainContainer">
           {/* Here we will be adding swiper container */}
           <div className="swiper-container">
             <div className="swiper-wrapper">
               {categoryData.map((el,index)=>{
                 const currentSection=this.state.category[this.state.index]
+
                 if(el===currentSection || currentSection==="All"){
                   return(
                     <div className="swiper-slide" key={index}>
-                      <div>{el}</div>
-                      <div class="imgCardHolder">
-                        <img src={`https://picsum.photos/id/102${index}/300/300.jpg`} alt="images"/>
+                      {/* <div>{el}</div> */}
+                      <div class="image-holder">
+                        <img src="http://www.coxreps.com/rf/image_medium/Pub/Web/CoxReps/Images/smartlocal-hero.jpg" alt="smart, local media. national scale." title="smart, local media. national scale." />
                       </div>
-                      <div>Demo Purposes</div>
+                      <div class="content-holder">
+                        <p class="headline">smart, local media. national scale.</p>
+                        <div class="list-text">
+                            <p>400+ best-in-class local broadcast television stations covering more than 87% of U.S. TV households</p>
+                        </div>
+                      </div>
+                      {/* <div class="imgCardHolder">
+                        <img src={`https://www.coxreps.com/rf/image_medium/Pub/Web/CoxReps/Images/experts-hero.jpg`} alt="images"/>
+                      </div> */}
+                      {/* <div>Demo Purposes</div> */}
                     </div>
                   )}
                   else{
                     return null
                   }
-              })}
+                }
+              )}
             </div>
             {/* -- If we need pagination -- */}
             <div class="swiper-pagination"></div>
